aa0517893bd8a5eb351e493b3b975ad0c5ee45ce
Tony Park <Tony.Park2@ontario.ca>
2020-07-28 16:15:54 -0400

    rollbacked profile auth part

--- gdct-app/frontend/src/App.js
+++ gdct-app/frontend/src/App.js
@@ -95,8 +95,8 @@ const App = () => {
           <PrivateRouter setLoggedIn={setLoggedIn} />
         </AuthPage>
       ) : (
-        <PublicRouter setLoggedIn={setLoggedIn} />
-      )}
+            <PublicRouter setLoggedIn={setLoggedIn} />
+          )}
     </div>
   );
 };
