bed7a26aac1676cb8c50cce82b158beedfd07442
George Chen <ggchen2k2@gmail.com>
2020-07-31 11:08:19 -0400

    Add CORS

--- gdct-app/backend/src/app.js
+++ gdct-app/backend/src/app.js
@@ -60,6 +60,11 @@ app.use(json({ limit: '50mb' }));
 app.use(urlencoded({ extended: true }));
 
 // app.use(cors());
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+})
 app.use(cors({ credentials: true, origin: process.env.CLIENT_SERVER }));
 
 app.use(compression());
