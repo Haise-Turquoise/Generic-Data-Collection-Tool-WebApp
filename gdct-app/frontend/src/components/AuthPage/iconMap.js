535380111680255f04fa2e78473fdb4131375f0e
David Zhu <C:\Users\ZhuDa2\AppData\Local\Programs\Microsoft VS Code>
2021-04-26 10:02:29 -0400

    overhaul modifyUserInfo page

--- gdct-app/frontend/src/components/AuthPage/iconMap.js
+++ gdct-app/frontend/src/components/AuthPage/iconMap.js
@@ -53,11 +53,6 @@ export default {
   'Modify Permission': <AccountCircleIcon style={SubMenuItemStyle} />,
   'Modify User Info': <AccountCircleIcon style={SubMenuItemStyle} />,
 
-  // // Modify Profile Test icon
-  // Profile1: <AccountCircleIcon style={MenuItemStyle} />,
-  // 'Modify Permission': <AccountCircleIcon style={SubMenuItemStyle} />,
-  // 'Modify User Info': <AccountCircleIcon style={SubMenuItemStyle} />,
-
   Logout: <ExitToAppIcon style={MenuItemStyle} />,
   // Main Menu Report
   Report: <ReportIcon />,
