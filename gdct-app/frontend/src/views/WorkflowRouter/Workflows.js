32ae07091ae0d32781b9fd4b6aea29ab87bff9b3
David Zhu <David.Zhu2@ontario.ca>
2021-06-01 10:52:41 -0400

    test husky uninstall

--- gdct-app/frontend/src/views/WorkflowRouter/Workflows.js
+++ gdct-app/frontend/src/views/WorkflowRouter/Workflows.js
@@ -42,7 +42,7 @@ const Workflows = () => {
     }),
     shallowEqual,
   );
-  // Convert Date format
+  // Convert date format
   workflows.forEach(workflow => {
     const logtime = new Date(workflow.timestamp);
     workflow.timestamp = moment(logtime).format("YYYY-MM-DD HH:mm:ss")
