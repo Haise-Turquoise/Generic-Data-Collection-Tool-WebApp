c48c0de6e091f3f053012148f154494811b548a6
Jie Yang <Jie.Yang2@ontario.ca>
2021-07-13 18:04:26 -0400

    Update entity.ts

--- gdct-app/backend/src/entities/User/entity.ts
+++ gdct-app/backend/src/entities/User/entity.ts
@@ -2,7 +2,7 @@ import { ObjectId } from "mongodb";
 import { UserDoc } from "../../types/user";
 
 export default class UserEntity {
-  public _id: ObjectId;
+  public _id: string;
   public username: string;
   public email: string;
   public title: string;
