89c70a2ad66870c4f9e717a514041a43e9ee5a30
Julien <jbertazzolambert@gmail.com>
2021-07-13 11:12:35 -0400

    hotfix

--- gdct-app/backend/src/controllers/index.ts
+++ gdct-app/backend/src/controllers/index.ts
@@ -33,7 +33,6 @@ import DataResumeController from './DataResume/controller';
 import SpreadsheetApisController from './GoogleApis';
 import TransferStatusController from './TransferStatus';
 import SessionController from '../controllers/Session/Session';
-import PackageStatusController from './PackageStatus/controller';
 
 export const routerManager = (app: Application) => {
   app.use('/', Container.get(AuthController));
@@ -84,8 +83,6 @@ export const routerManager = (app: Application) => {
   // Use to handle transfer control
   
   app.use('/transferManager', Container.get(TransferStatusController));
-
-  app.use('/report', Container.get(PackageStatusController))
 
 };
 
