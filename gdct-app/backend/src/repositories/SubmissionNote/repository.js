b23992c34ba512ddf26ac8c03b8e3d938230402b
Julien <jbertazzolambert@gmail.com>
2021-07-13 11:24:55 -0400

    File created for merge conflict

---
+++ gdct-app/backend/src/repositories/SubmissionNote/repository.js
@@ -0,0 +1,22 @@
+import SubmissionNoteEntity from '../../entities/SubmissionNote/SubmissionNote';
+import BaseRepository from '../repository';
+import SubmissionNoteModel from '../../models/SubmissionNote';
+
+export default class SubmissionNoteRepository extends BaseRepository {
+  constructor() {
+    super(SubmissionNoteModel);
+  }
+
+  async create(submissionNote) {
+    console.log(submissionNote);
+    return SubmissionNoteModel.create(submissionNote);
+  }
+
+  async findBySubmissionId(submissionId) {
+    return SubmissionNoteModel.find({ submissionId });
+  }
+
+  async updateNoteToNewSubmission(oldSubmissionId, newSubmissionId){
+    return SubmissionNoteModel.updateMany({submissionId:oldSubmissionId}, {$set:{submissionId:newSubmissionId}});
+  }
+}
