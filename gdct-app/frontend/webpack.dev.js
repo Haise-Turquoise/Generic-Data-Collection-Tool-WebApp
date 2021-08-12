313cc19ce814c5c86f496c8c680bf49a53b2c0da
Changlin Su <41277350+Sheldonsu28@users.noreply.github.com>
2021-08-12 11:11:11 -0400

    update docker compose and docker config

--- gdct-app/frontend/webpack.dev.js
+++ gdct-app/frontend/webpack.dev.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: '.',
-    // host:'0.0.0.0',
+    host:'gdct-container.azurewebsites.net',
     hot: true,
     port: 3003,
     open: true,
