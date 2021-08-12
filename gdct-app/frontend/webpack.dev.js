73e2e501a74ce5abf05933f7efe7b6dd404f74bb
Changlin Su <41277350+Sheldonsu28@users.noreply.github.com>
2021-08-12 12:11:05 -0400

    Update webpack.dev.js

--- gdct-app/frontend/webpack.dev.js
+++ gdct-app/frontend/webpack.dev.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: '.',
-    host:'gdct-container.azurewebsites.net',
+    host:'0.0.0.0',
     hot: true,
     port: 3003,
     open: true,
