698d875384a6e5cc1a54c3f6b5b631aa8f1b92f7
Changlin Su <41277350+Sheldonsu28@users.noreply.github.com>
2021-08-13 14:00:25 -0400

    Update webpack.dev.js

--- gdct-app/frontend/webpack.dev.js
+++ gdct-app/frontend/webpack.dev.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
   devServer: {
     contentBase: '.',
     host:'0.0.0.0',
-    hot: true,
+    // hot: true,
     port: 3003,
     open: true,
     historyApiFallback: true,
