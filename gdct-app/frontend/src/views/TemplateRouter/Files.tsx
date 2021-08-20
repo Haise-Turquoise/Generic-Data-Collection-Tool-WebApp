b6af23987a50b0f6a0d826e073eddc5c94f747d6
Julien <jbertazzolambert@gmail.com>
2021-08-20 15:52:50 -0400

    final hotfix

--- gdct-app/frontend/src/views/TemplateRouter/Files.tsx
+++ gdct-app/frontend/src/views/TemplateRouter/Files.tsx
@@ -1,17 +1,12 @@
 import { Button } from '@material-ui/core'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import path from 'path'
 
 export default function Files() {
-  const [url, setUrl] = useState<string | undefined>()
-
-  useEffect(() => {
-    FileController.offlineTool().then(setUrl)
-  }, [])
 
   return (
     <div>
-      <Button download="GT5.xlam" href={path.join(__dirname, '../../../public/GT5.xlam')} disabled={!url} color="primary" variant="contained">{url ? 'Download' : 'Loading'}</Button>
+      <Button download="GT5.xlam" href={path.join(__dirname, '../../../public/GT5.xlam')} color="primary" variant="contained">Download</Button>
     </div>
   )
 }
