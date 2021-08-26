080b2633c5551cae62767e76ab7f71829edd59fb
Changlin Su <41277350+Sheldonsu28@users.noreply.github.com>
2021-08-26 09:06:30 -0400

    Update template.ts

--- gdct-app/backend/src/types/template.ts
+++ gdct-app/backend/src/types/template.ts
@@ -10,7 +10,6 @@ export default interface Template {
   userCreatorId: ObjectId;
   creationDate: Date;
   createdAt: Date;
-  updatedAt: Date;
   expirationDate: Date;
   statusId: ObjectId;
   googleSheetId: ObjectId;
