4f2571fd46c76288a91b0a3dfeae87148f640d27
IbrahimAbdi1 <Ibrahim_abdi57@hotmail.com>
2021-11-02 15:46:22 -0400

    moved logo and program bug fix

--- gdct-app/frontend/src/views/GDCTMenu/GDCTMenu.tsx
+++ gdct-app/frontend/src/views/GDCTMenu/GDCTMenu.tsx
@@ -1,19 +1,13 @@
 import React, { useState, useEffect, MouseEventHandler, MouseEvent } from 'react';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { Button, SvgIconProps, SvgIconTypeMap } from '@material-ui/core';
 //@ts-ignore
 import createUserNavigation from '../../components/AuthPage/createUserNavigation';
 //@ts-ignore
 import DrawerItem from '../../components/AuthPage/DrawerItem';
-import { selectFactoryRESTResponseTableValues } from '../../store/common/REST/selectors';
 //@ts-ignore
 import IconItem from '../../components/AuthPage/IconItem';
 import { OverridableComponent } from '@material-ui/core/OverridableComponent';
-import { selectSubmissionsStore } from '../../store/SubmissionsStore/selectors';
-import { getSubmissionsRequest } from '../../store/thunks/submission';
-import { Submission } from '../../types/submissions'
 import { MappedMenu } from '../../types/menu';
-import './menuStyle.css';
 
 const MenuHeader = () => {
   const [anchorEl, setAnchorEl] = React.useState<EventTarget | null>(null);
@@ -30,7 +24,6 @@ const MenuHeader = () => {
     <>
       {config.map((item, index) => {
         const { type, name, icon, url } = item;
-        console.log(url);
         return item.type !== 'drawer' && item.type !== 'topmenu' ? (
           // @ts-ignore
           <IconItem key={`${type}-${name}-${index}`} name={name} url={url} icon={icon} />
@@ -61,41 +54,17 @@ const MenuHeader = () => {
 };
 
 const GDCTMenu = () => {
-  const dispatch = useDispatch();
-  const [readMessage, setMessage] = useState('Loading submissions...');
-
-  let { submissions }:{submissions:Submission[]} = useSelector(
-    state => ({
-      submissions: selectFactoryRESTResponseTableValues(selectSubmissionsStore)(state),
-    }),
-    shallowEqual,
-  )
-
-  if (submissions.length == 0) {
-    
-    console.log("hit");
-    dispatch(getSubmissionsRequest(()=>{setMessage('Nothing to show');}));
-  }
-
-
-  useEffect(()=>{
-    console.log("hi");
-    console.log(submissions);
-  },[submissions])
-
   return (
-
-    <div>
-      <p>Welcome back to GDCT</p>
-
-      <div className="subDasboard">
-        <div className="subDasboardTitle">
-          <div className="subleft">Submission Dashboard To-do..</div>
-          <div className="subright"> <a> See more in submission dashboard</a></div>
-        </div>
-        
-      </div>
-       
+    <div
+      style={{
+        maxWidth: '1280px',
+        margin: '0 auto',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(400px, auto))',
+        gap: '1em',
+      }}
+    >
+      <MenuHeader />
     </div>
   );
 };
