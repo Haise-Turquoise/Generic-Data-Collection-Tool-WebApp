250b9d23b4ca1a92e8483d583f40ebcaf7437235
Brandon Liang <Brandon.Liang3@ontario.ca>
2022-06-14 23:14:44 +0000

    Revert "Merged PR 496: Revert "Merged PR 494: 4378, prepopulate templates
    Revert "Merged PR 494: 4378, prepopulate templates
    
    Related work items: #4378"
    
    Reverted commit `ffa737ba`."

--- gdct-app/frontend/src/views/TemplateRouter/Template/spreadSheet.tsx
+++ gdct-app/frontend/src/views/TemplateRouter/Template/spreadSheet.tsx
@@ -30,6 +30,7 @@ import Attribute from "../../../types/attrubute";
 import UpdatePeriod from "./UpdatePeriod";
 import { CodeSharp } from "@material-ui/icons";
 import swal from 'sweetalert2'
+import spreadsheetInitialize from "./spreadSheetHelper";
 
 // Sheet style Option
 const sheetOption = {
@@ -145,6 +146,10 @@ class SpreadSheet extends Component<SpreadSheetProps, {hasSheet: boolean}>{
       this.sheet.on('cell-selected',(cell:object, row:number, col:number)=>{
         this.currentCoord = {row, col};
       })
+      
+
+      //the following is to prepopulate the spreadsheet with some an existing format
+      spreadsheetInitialize(this.sheet);
     });
 
     // fetch Validation Threshold
@@ -418,6 +423,9 @@ class SpreadSheet extends Component<SpreadSheetProps, {hasSheet: boolean}>{
     if (!mainMenu) {
       return ""
     }    
+    
+    // mainMenu.setCellText(4, 1, "Year:", 'finished');
+    // mainMenu.setCellText(5, 1, "Quarter:", 'finished');
     const year = mainMenu.getCellTextOrDefault(4, 2);
     const q = mainMenu.getCellTextOrDefault(5, 2);
     return !!q ? `${year} ${q}` : `${year}`;
