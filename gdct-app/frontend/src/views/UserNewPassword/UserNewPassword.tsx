d0c9d76e82b78c31d63ac8bf258770cdc58fcc7b
Yufei Chen <Yufei.Chen2@ontario.ca>
2022-11-07 13:17:31 -0500

    update password v1

---
+++ gdct-app/frontend/src/views/UserNewPassword/UserNewPassword.tsx
@@ -0,0 +1,230 @@
+//@ts-ignore
+import React, { useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import { Formik } from 'formik';
+//@ts-ignore
+import Swal, { SweetAlertResult } from 'sweetalert2';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+//@ts-ignore
+import Typography from '@material-ui/core/Typography';
+
+import './NewPassword.scss';
+import Box from '@material-ui/core/Box';
+//@ts-ignore
+import * as yup from 'yup';
+
+import {
+  submit,
+  //@ts-ignore
+} from '../../store/thunks/userRegistration';
+//@ts-ignore
+import UserController from '../../controllers/user';
+
+// The schema to validate user input
+const registerSchema = () =>
+  yup.object().shape({
+    password: yup
+      .string()
+      .min(8, 'The given password is too short. Password must be at least 8 character(s) long')
+      .matches(
+        /[{0-9}]/,
+        'Password has too few numeric characters (0-9). The password must have at least 1 numeric character(s)',
+      )
+      .matches(
+        /[{a-z}{A-Z}}]/,
+        'Password has too few alphabetic characters (A-Z, a-z). The password must have at least 2 alphabetic character(s)',
+      )
+      .required('Please enter a password'),
+    passwordConfirm: yup
+      .string()
+      .oneOf([yup.ref('password'), undefined], 'Password should match with Verify Password')
+      .required('Please confirm your password'),
+    email: yup
+      .string()
+      .email('Please enter a valid email')
+      .max(254, 'Email is too long')
+      .required('Please enter your email'),
+  });
+
+// Button on the bottom of page
+const ButtonBox = ({ handleSubmit }: { handleSubmit: () => void }) => (
+  <Box border={1} color="primary" className="register__buttonBox" justifyContent="center">
+
+    <Button variant="outlined" color="primary" className="register__button" href="/login">
+      Cancel
+    </Button>
+
+    <Button
+      variant="outlined"
+      color="primary"
+      className="register__button"
+      onClick={handleSubmit}
+    >
+      Update
+    </Button>
+    <br />
+    <br />
+    <Typography className="register__subtext" >
+      Please use the navigation buttons on this page to move between the registration steps.
+      Using the browser's Back and Forward buttons will remove the inputted information.
+    </Typography>
+  </Box>
+);
+
+// Have the detail UI page for each step
+const getStepContent = (
+  handleSubmit: () => void,
+  props: any,
+) => {
+  const { values, handleChange, touched, handleBlur, errors, isValid } = props;
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <form className="register__form">
+        <br />
+        <div className="register__label">
+          <Typography className="register__inputTitle"> *Email </Typography>
+        </div>
+        <div className="register__informationField">
+          <TextField
+            variant="outlined"
+            className="register__field"
+            id="email"
+            name="email"
+            type="email"
+            value={values.email}
+            onChange={handleChange}
+            error={touched.email && !!errors.email}
+            helperText={touched.email && errors.email}
+            onBlur={handleBlur}
+            InputProps={{
+              style: {
+                height: 50,
+                backgroundColor: 'aliceblue',
+              },
+            }}
+          />
+        </div>
+        <br />
+        <div className="register__label">
+          <Typography className="register__inputTitle"> *Password </Typography>
+        </div>
+        <div className="register__informationField">
+          <TextField
+            variant="outlined"
+            className="register__field"
+            id="password"
+            name="password"
+            type="password"
+            autoComplete="new-password"
+            value={values.password}
+            onChange={handleChange}
+            error={touched.password && !!errors.password}
+            helperText={touched.password && errors.password}
+            onBlur={handleBlur}
+            InputProps={{
+              style: {
+                height: 50,
+                backgroundColor: 'aliceblue',
+              },
+            }}
+          />
+        </div>
+        <br />
+        <div className="register__label">
+          <Typography className="register__inputTitle"> *Confirm Password </Typography>
+        </div>
+        <div className="register__informationField">
+          <TextField
+            variant="outlined"
+            className="register__field"
+            id="passwordConfirm"
+            name="passwordConfirm"
+            type="password"
+            value={values.passwordConfirm}
+            onChange={handleChange}
+            error={touched.passwordConfirm && !!errors.passwordConfirm}
+            helperText={touched.passwordConfirm && errors.passwordConfirm}
+            onBlur={handleBlur}
+            InputProps={{
+              style: {
+                height: 50,
+                backgroundColor: 'aliceblue',
+              },
+            }}
+          />
+        </div>
+        <br />
+        <ButtonBox
+          handleSubmit={handleSubmit}
+        />
+      </form>
+    </div>
+  );
+};
+
+// Get the state and shown it on the website
+const Register_container = (props: any) => {
+  console.log(props)
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleSubmit = useCallback(() => {
+    dispatch(submit());
+    Swal.fire({
+      title: 'Success!',
+      text: 'Your request has been submitted',
+      icon: 'success',
+      confirmButtonColor: '#3085d6',
+      confirmButtonText: 'OK',
+    }).then((result: SweetAlertResult<any>) => {
+      if (result.isConfirmed) {
+        // window.location.reload();
+        history.push('/');
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      {getStepContent(
+        handleSubmit,
+        props,
+      )}
+    </div>
+  );
+};
+
+// Main function to export
+const Register = () => {
+  const handleSubmit = () => { };
+  const dispatch = useDispatch();
+  const { registrationData } = useSelector(
+    // @ts-ignore
+    ({ UserRegistrationStore: { registrationData } }) => ({
+      registrationData,
+    }),
+    shallowEqual,
+  );
+
+  return (
+    <div>
+      {/* <SRIHeader/> */}
+      <div className="register">
+        <br />
+        <Paper className="register__container">
+          <Formik
+            validationSchema={registerSchema}
+            initialValues={registrationData}
+            onSubmit={handleSubmit}
+            render={formikProps => <Register_container {...formikProps} />}
+          />
+        </Paper>
+      </div>
+    </div>
+  );
+};
+export default Register;
