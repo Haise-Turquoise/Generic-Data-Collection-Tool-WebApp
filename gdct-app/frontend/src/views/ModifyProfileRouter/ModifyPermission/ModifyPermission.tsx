1f8c3fa33e1af5d560ed2a1746c1a9b3e53757e3
Yuanhao(Jim) Liu <Yuanhao.Liu@ontario.ca>
2023-03-14 14:53:46 -0400

    add headling on user-profile-period permissions

--- gdct-app/frontend/src/views/ModifyProfileRouter/ModifyPermission/ModifyPermission.tsx
+++ gdct-app/frontend/src/views/ModifyProfileRouter/ModifyPermission/ModifyPermission.tsx
@@ -597,6 +597,14 @@ const ModifyPermission_container = (props:any) => {
   );
 };
 
+const SubmissionPermissionsHeader = () => {
+  return (
+    <Paper className="header">
+      <Typography variant="h5">Submission Permissions</Typography>
+    </Paper>
+  );
+};
+
 // Main function to export
 const ModifyPermission = () => {
   const handleSubmit = () => {};
@@ -615,8 +623,8 @@ const ModifyPermission = () => {
   );
 
   return (
-    <>
-      {/* <SRIHeader/> */}
+    <div>
+      <SubmissionPermissionsHeader/>
       <div className="modifyPermission">
         <br />
         <Paper className="modifyPermission__container">
@@ -627,7 +635,7 @@ const ModifyPermission = () => {
           />
         </Paper>
       </div>
-    </>
+    </div>
   );
 };
 export default ModifyPermission;
