72d2269f85ca7c94bde056636c3d9d1ebb6615f5
Jim Liu <yuanhao.liu@ontario.ca>
2023-03-22 12:34:24 -0400

    add filter to extra material table

--- gdct-app/frontend/src/views/RoleRouter/AppResourceList/AppResourceList.tsx
+++ gdct-app/frontend/src/views/RoleRouter/AppResourceList/AppResourceList.tsx
@@ -78,7 +78,7 @@ const AppResourceList = ({ resourceId, isEditable = true, onClickAdd, onClickDel
       showTitle: true,
       maxBodyHeight: '400px',
       minBodyHeight: '400px',
-      filtering: true
+      filtering: true,
     }),
     [],
   );
